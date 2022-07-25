638525a69b04735753870c342596ee1bfd0822af
Advay Iyer <Advay@protonmail.com>
2022-07-25 16:27:46 +0100

    Implemented isenaccounter v2 for all bits of information present in i

--- ThermoSolverV2/propertiesaround.py
+++ ThermoSolverV2/propertiesaround.py
@@ -17,17 +17,27 @@ def checkaround(properties, processes,plotinfo, definedstates,newinfo, i):
 
             if properties[nextstate][0] != 0 and processes[i][2] == 1:
                 # We can only use T to fully define the system when it is isentropic, otherwise the other property we can calculate must be enthalpy, and enthalpy and temprerature alone is not enough to define the perfect gas state.
+
                 newinfo = True
                 properties[nextstate][1] = (1/(gamma-1)) * math.log((properties[i][0]* (properties[i][2]**(gamma-1)))/properties[nextstate][0])
                                                                                                                 # We want to fully define the isentropic matrix to allow us to use its values if it turns out to be good, this could also aid in drawing graphs where we could use the isentropic case to show how isentropic efficencies are affecting the graph, sicne the isentropic stuff updates each tijme perhaps we need a "saved" one which keeps the isentropic function rather than updating with the main so we can seperate what was calculated isentropically and graph it distinctly
                 properties[nextstate] = FullyDefinernew(isenproperties[nextstate])
 
+                plotinfo[i][0] = properties[i]
+                plotinfo[nextstate][0] = properties[nextstate]
+                plotinfo[i][accuracy - 1] = properties[nextstate]
+                intermediates = np.linspace(plotinfo[i][0][0], plotinfo[nextstate][0][0],num=accuracy)  # here we solve for P becuause this is the data that was already there, the rest of the data will be solved off this.
+                plotinfo[i][:, 0] = intermediates
+
+
+                for j in range(0, accuracy):
+                    if processes[i][2] ==1:
+                        plotinfo[i][j][1] = (plotinfo[i][j][0]/properties[i][0] * (properties[i][1])**((gamma-1)/gamma))**(gamma/(gamma-1))
+                        plotinfo[i][j]=FullyDefinernew(plotinfo[i][j])
 
 
 
 
-
-                # Now that we have caluclated some new properties we outhgt to try and plot what is going on... It could ctreate a real problem if the system is overdefcined, even if it is correct if two states are fully defined and adjacent from the outset we are not going to be able to plot the stuff inbetween
 
             elif properties[nextstate][1] !=0 and processes[i][2]!=0:
 
@@ -39,10 +49,24 @@ def checkaround(properties, processes,plotinfo, definedstates,newinfo, i):
 
                 properties[nextstate] = isenaccounterv2(properties[nextstate],properties[i],i,processes)
 
+                plotinfo[i][0] = properties[i]
+                plotinfo[nextstate][0] = properties[nextstate]
+                plotinfo[i][accuracy - 1] = properties[nextstate]
+                intermediates = np.linspace(plotinfo[i][0][1], plotinfo[nextstate][0][1],num=accuracy)  # here we solve for P becuause this is the data that was already there, the rest of the data will be solved off this.
+                plotinfo[i][:, 1] = intermediates
 
-                # plotinfo[i][0] = properties[i]
-                # plotinfo[i][accuracy - 1] = properties[nextstate]
-                # plotinfo[nextstate][0] = properties[nextstate]
+
+
+
+                for j in range(0, accuracy):
+                    if processes[i][2] !=0:
+
+                        plotinfo[i][j][0] = properties[i][0] * (plotinfo[i][j][1]/properties[i][1])**((gamma-1)/gamma)
+                        plotinfo[i][j] = mgetH(plotinfo[i][j],0)
+                        plotinfo[i][j] = isenaccounterv2(plotinfo[i][j],properties[i],i,processes)
+
+
+
 
             elif properties[nextstate][2]!=0 and processes[i][2]!=0:
                 newinfo = True
@@ -53,9 +77,22 @@ def checkaround(properties, processes,plotinfo, definedstates,newinfo, i):
 
                 properties[nextstate] = isenaccounterv2(properties[nextstate], properties[i], i, processes)
 
-            # plotinfo[i][0] = properties[i]
-                # plotinfo[i][accuracy - 1] = properties[nextstate]
-                # plotinfo[nextstate][0] = properties[nextstate]
+                plotinfo[i][0] = properties[i]
+                plotinfo[nextstate][0] = properties[nextstate]
+                plotinfo[i][accuracy - 1] = properties[nextstate]
+                intermediates = np.linspace(plotinfo[i][0][2], plotinfo[nextstate][0][2],num=accuracy)  # here we solve for P becuause this is the data that was already there, the rest of the data will be solved off this.
+                plotinfo[i][:, 2] = intermediates
+
+
+
+                for j in range(0, accuracy):
+                    if processes[i][2] != 0:
+
+                        plotinfo[i][j][0] = properties[i][0]*(properties[i][2]/plotinfo[i][j][2])**(gamma-1)
+                        plotinfo[i][j] = mgetH(plotinfo[i][j], 0)
+                        plotinfo[i][j] = isenaccounterv2(plotinfo[i][j], properties[i], i, processes)
+
+
 
 
 
@@ -65,16 +102,20 @@ def checkaround(properties, processes,plotinfo, definedstates,newinfo, i):
                 newinfo = True
 
                 properties[prevstate][1] = (1/(gamma-1)) * math.log((properties[i][0]* (properties[i][2]**(gamma-1)) )/properties[prevstate][0])
-
-
                 properties[prevstate] = FullyDefinernew(isenproperties[prevstate])
 
+                plotinfo[i][0] = properties[i]
+                plotinfo[prevstate][0] = properties[prevstate]
+                plotinfo[prevstate][accuracy - 1] = properties[i]
+                intermediates = np.linspace(plotinfo[prevstate][0][1], plotinfo[i][0][1],num=accuracy)  # here we solve for P becuause this is the data that was already there, the rest of the data will be solved off this.
+                plotinfo[prevstate][:, 1] = intermediates
+
+                for j in range(0,accuracy):
+                    newinfo = True
+                    plotinfo[prevstate][j][1] = (1 / (gamma - 1)) * math.log((properties[i][0] * (properties[i][2] ** (gamma - 1))) / plotinfo[prevstate][j][0])
+                    plotinfo[prevstate][j] = FullyDefinernew(plotinfo[prevstate[j]])
 
 
-
-                # plotinfo[i][0] = properties[i]
-                # plotinfo[prevstate][accuracy-1] = properties[i]
-                # plotinfo[prevstate][0] = properties[prevstate]
 
 
             elif properties[prevstate][1] != 0 and processes[prevstate][2]==1 and definedstates[prevstate] == False:
@@ -84,29 +125,26 @@ def checkaround(properties, processes,plotinfo, definedstates,newinfo, i):
                 properties[prevstate][0] = properties[i][0] * (properties[prevstate][1] / properties[i][1]) ** ((gamma - 1) / gamma)
                 properties[prevstate] = FullyDefinernew(properties[prevstate])
 
-
-
-
-
-
-
-
-                                    # Im going to modularise this algorythm and place it as a functiom within a different module.
                 plotinfo[i][0] = properties[i]
                 plotinfo[prevstate][0] = properties[prevstate]
                 plotinfo[prevstate][accuracy - 1] = properties[i]
                 intermediates = np.linspace(plotinfo[prevstate][0][1],plotinfo[i][0][1],num=accuracy)                 # here we solve for P becuause this is the data that was already there, the rest of the data will be solved off this.
                 plotinfo[prevstate][:,1] = intermediates
 
-                for j in range(0,accuracy):
-                    if processes[prevstate][2] != 0:                             # We need to account for the fact that if the info is about T then it only works if isentropic!
-                        newinfo = True
-                        plotinfo[prevstate][j][0] = properties[i][0] * (plotinfo[prevstate][j][1]/properties[i][1])**((gamma - 1) / gamma)  # here we are storing the isentropic Tempreturues, however now we need to account for isentropic efficiency and store the real tempreratures
-                        isenindex = 0                                                                                   # Setting a vairable to track which property has the isentropic assumption in order to remove it once the algorythm has calculated h
-                        plotinfo[prevstate][j] = mgetH(plotinfo[prevstate][j],isenindex)                                        # We don't actually want to define the whole thing herre, what we need is an algorythm that will just get us h!
-                        plotinfo[prevstate][j] = isenaccounterv2(plotinfo[prevstate][j],properties[i],prevstate,processes)
-                current,prevprevstate = adjstates(prevstate)
-                plotinfo[prevprevstate][accuracy-1] = plotinfo[prevstate][0]                                            # This alogrytm is really nice hiowever it really does not make any sense to use it in thw backwards fashion
+                for j in range(0, accuracy):
+                    newinfo = True
+                    plotinfo[prevstate][j][0] = properties[i][0] * (plotinfo[prevstate][j][1]/properties[i][1])**((gamma - 1) / gamma)
+                    plotinfo[prevstate][j] = FullyDefinernew(plotinfo[prevstate][j])
+
+                # for j in range(0,accuracy):
+                #     if processes[prevstate][2] != 0:                             # We need to account for the fact that if the info is about T then it only works if isentropic!
+                #         newinfo = True
+                #         plotinfo[prevstate][j][0] = properties[i][0] * (plotinfo[prevstate][j][1]/properties[i][1])**((gamma - 1) / gamma)  # here we are storing the isentropic Tempreturues, however now we need to account for isentropic efficiency and store the real tempreratures
+                #         isenindex = 0                                                                                   # Setting a vairable to track which property has the isentropic assumption in order to remove it once the algorythm has calculated h
+                #         plotinfo[prevstate][j] = mgetH(plotinfo[prevstate][j],isenindex)                                        # We don't actually want to define the whole thing herre, what we need is an algorythm that will just get us h!
+                #         plotinfo[prevstate][j] = isenaccounterv2(plotinfo[prevstate][j],properties[i],prevstate,processes)
+                # current,prevprevstate = adjstates(prevstate)
+                # plotinfo[prevprevstate][accuracy-1] = plotinfo[prevstate][0]                                            # This alogrytm is really nice hiowever it really does not make any sense to use it in thw backwards fashion
 
 
 
@@ -118,74 +156,20 @@ def checkaround(properties, processes,plotinfo, definedstates,newinfo, i):
 
                 properties[prevstate] = FullyDefinernew(isenproperties[prevstate])
 
+                plotinfo[i][0] = properties[i]
+                plotinfo[prevstate][0] = properties[prevstate]
+                plotinfo[prevstate][accuracy - 1] = properties[i]
+                intermediates = np.linspace(plotinfo[prevstate][0][1], plotinfo[i][0][1],num=accuracy)  # here we solve for P becuause this is the data that was already there, the rest of the data will be solved off this.
+                plotinfo[prevstate][:, 1] = intermediates
 
+                for j in range(0, accuracy):
+                    newinfo = True
+                    plotinfo[prevstate][j][1] = properties[i][1] * (properties[i][2] / plotinfo[prevstate][j][2]) ** (gamma)
+                    plotinfo[prevstate][j] = FullyDefinernew(plotinfo[prevstate[j]])
 
-                # plotinfo[i][0] = properties[i]
-                # plotinfo[prevstate][accuracy - 1] = properties[i]
-                # plotinfo[prevstate][0] = properties[prevstate]
-                #
-                # intermediates = np.linspace(plotinfo[prevstate][0][2], plotinfo[i][0][2], num=10)
-                #
-                # plotinfo[prevstate][:, 2] = intermediates
-                #
-                # for j in range(0,accuracy):
-                #
-                #     plotinfo[prevstate][j][0] = properties[i][0] * (plotinfo[prevstate][j][1]/properties[i][1])**((gamma - 1) / gamma)
-                #     plotinfo[prevstate] = FullyDefiner(plotinfo[prevstate],j)
 
     return properties,plotinfo,newinfo
 
 
 
-def intermediatestatesP(properties,plotinfo,newinfo,i,proptype,processes):
-    nextstate,prevstate = adjstates(i)
 
-    # Im going to modularise this algorythm and place it as a functiom within a different module.
-    plotinfo[i][0] = properties[i]
-    plotinfo[prevstate][0] = properties[prevstate]
-    plotinfo[prevstate][accuracy - 1] = properties[i]
-    intermediates = np.linspace(plotinfo[prevstate][0][proptype], plotinfo[i][0][proptype],num=accuracy)  # here we solve for P becuause this is the data that was already there, the rest of the data will be solved off this.
-    plotinfo[prevstate][:, proptype] = intermediates
-
-
-
-    for j in range(0, accuracy):
-
-        if processes[prevstate][2] != 0:  # We need to account for the fact that if the info is about T then it only works if isentropic!
-            newinfo = True
-            plotinfo[prevstate][j][0] = properties[i][0] * (plotinfo[prevstate][j][1] / properties[i][1]) ** ((gamma - 1) / gamma)  # here we are storing the isentropic Tempreturues, however now we need to account for isentropic efficiency and store the real tempreratures
-
-            isenindex = 0  # Setting a vairable to track which property has the isentropic assumption in order to remove it once the algorythm has calculated h
-            plotinfo[prevstate][j] = mgetH(plotinfo[prevstate][j],isenindex)  # We don't actually want to define the whole thing herre, what we need is an algorythm that will just get us h!
-            plotinfo[prevstate][j] = isenaccounterv2(plotinfo[prevstate][j], properties[i], prevstate, processes)
-    current, prevprevstate = adjstates(prevstate)
-    plotinfo[prevprevstate][accuracy - 1] = plotinfo[prevstate][0]  # This alogrytm is really nice hiowever it really does not make any sense to use it in thw backwards fashion
-
-    return properties,plotinfo,newinfo
-
-
-def intermediatestatesv(properties, plotinfo, newinfo, i, proptype, processes):
-    nextstate, prevstate = adjstates(i)
-
-    # Im going to modularise this algorythm and place it as a functiom within a different module.
-    plotinfo[i][0] = properties[i]
-    plotinfo[prevstate][0] = properties[prevstate]
-    plotinfo[prevstate][accuracy - 1] = properties[i]
-    intermediates = np.linspace(plotinfo[prevstate][0][proptype], plotinfo[i][0][proptype],num=accuracy)  # here we solve for P becuause this is the data that was already there, the rest of the data will be solved off this.
-    plotinfo[prevstate][:, proptype] = intermediates
-
-    for j in range(0, accuracy):
-
-        if processes[prevstate][
-            2] != 0:  # We need to account for the fact that if the info is about T then it only works if isentropic!
-            newinfo = True
-            plotinfo[prevstate][j][0] = properties[i][0] * (plotinfo[prevstate][j][1] / properties[i][1]) ** (( gamma - 1) / gamma)  # here we are storing the isentropic Tempreturues, however now we need to account for isentropic efficiency and store the real tempreratures
-
-
-            isenindex = 0  # Setting a vairable to track which property has the isentropic assumption in order to remove it once the algorythm has calculated h
-            plotinfo[prevstate][j] = mgetH(plotinfo[prevstate][j],isenindex)  # We don't actually want to define the whole thing herre, what we need is an algorythm that will just get us h!
-            plotinfo[prevstate][j] = isenaccounterv2(plotinfo[prevstate][j], properties[i], prevstate, processes)
-    current, prevprevstate = adjstates(prevstate)
-    plotinfo[prevprevstate][accuracy - 1] = plotinfo[prevstate][0]  # This alogrytm is really nice hiowever it really does not make any sense to use it in thw backwards fashion
-
-    return properties, plotinfo, newinfo
