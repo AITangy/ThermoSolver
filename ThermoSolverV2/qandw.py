fc9ec871d9c8b38c5db474385e74731b76658ce8
Advay Iyer <Advay@protonmail.com>
2022-08-01 12:17:18 +0100

    Added a way to indicate when an SFEE variable is not 0 but not known, and hence not conduct calculation.

--- ThermoSolverV2/qandw.py
+++ ThermoSolverV2/qandw.py
@@ -22,12 +22,17 @@ def calcqandw(properties,processes,i):
 def useqandw(properties,processes,i):
 
     nextstate, prevstate = statename.adjstates(i)
-    if processes[i][0]!="" and processes[i][1]!="":
+    sufficientinfo = False
+    if processes[i][3]!="" and processes[i][6]!="" and processes[i][7]!="" and processes[nextstate][3]!="" and processes[nextstate][6]!="" and processes[nextstate][7]!="":
+        sufficientinfo = True
+
+
+    if processes[i][0]!="" and processes[i][1]!="" and sufficientinfo==True:
         if properties[i][4]!=0 and properties[nextstate][4]==0:                                                 # Solving if we have knowledge on enthalpy of current state
             properties[nextstate][4] = (processes[i][0] - processes[i][1] + processes[i][3] * (properties[i][4]) + 0.5 * (processes[i][6])**2 + g * processes[i][7])/processes[nextstate][3] - 0.5 * (processes[nextstate][6])**2 - g*processes[nextstate][7]
 
 
-        if properties[nextstate][4]!=0 and properties[i][4] == 0:                                               # Solving if we have knowledge on enthalpy of previous state
+        if properties[nextstate][4]!=0 and properties[i][4] == 0 and sufficientinfo==True:                                               # Solving if we have knowledge on enthalpy of previous state
             properties[i][4] = (processes[i][0] - processes[i][1] + processes[i][3]*(processes[i][4] + 0.5 * processes[i][6]**2 + g*processes[i][7]))/processes[nextstate][3] - 0.5*processes[nextstate][6]**2 - g*processes[nextstate][7]
 
     return properties,processes
